81dad7475743755e87bc430a5c13e7692ca7e7ee
Salad109 <mateuz204@gmail.com>
2025-06-06 22:23:07 +0200

    Initial commit

---
+++ src/main/java/org/example/myproject/HelloController.java
@@ -0,0 +1,32 @@
+package org.example.myproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World! My first Spring Boot app is working!";
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/api/user")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/api/user/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
