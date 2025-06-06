3687d008581cf38f4c9e8b844140ae36274efa47
Salad109 <szolek1337@gmail.com>
2025-06-06 22:23:07 +0200

    Initial commit

---
+++ src/main/java/org/example/myproject/User.java
@@ -0,0 +1,70 @@
+package org.example.myproject;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String city;
+
+    @Embedded
+    private Email email;
+
+    public User() {
+    }
+
+    // Constructor (don't include id - database will generate it)
+    public User(String name, int age, String city, String emailString) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.email = new Email(emailString);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+}
+
