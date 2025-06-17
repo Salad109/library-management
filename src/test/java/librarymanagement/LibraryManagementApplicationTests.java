519f2b60aaa2285db5b1cca2ebcbf19c553a16eb
Salad109 <szolek1337@gmail.com>
2025-06-18 00:31:08 +0200

    Fix Maven and Java version

--- src/test/java/librarymanagement/LibraryManagementApplicationTests.java
+++ src/test/java/librarymanagement/LibraryManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.myproject;
+package librarymanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyprojectApplicationTests {
+class LibraryManagementApplicationTests {
 
     @Test
     void contextLoads() {
