3719e1f0779f9da4e0d34f7efc7a548a3d902273
Salad109 <szolek1337@gmail.com>
2025-06-18 15:54:26 +0200

    Add unit tests for Book and BookController classes

---
+++ src/test/java/librarymanagement/model/BookTest.java
@@ -0,0 +1,41 @@
+package librarymanagement.model;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookTest {
+
+    @Test
+    void testBookConstructor() {
+        String title = "Test Book";
+        String author = "Joe Mama";
+        Integer publicationYear = 2025;
+        String isbn = "1234567890";
+
+        Book book = new Book(title, author, publicationYear, isbn);
+
+        assertThat(book.getTitle()).isEqualTo(title);
+        assertThat(book.getAuthor()).isEqualTo(author);
+        assertThat(book.getPublicationYear()).isEqualTo(publicationYear);
+        assertThat(book.getIsbn()).isEqualTo(isbn);
+    }
+
+    @Test
+    void testBookSetters() {
+        Book book = new Book();
+        String title = "Test Book 2";
+        String author = "Jane Mama";
+        Integer publicationYear = 2077;
+        String isbn = "9781234567890";
+
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublicationYear(publicationYear);
+        book.setIsbn(isbn);
+
+        assertThat(book.getTitle()).isEqualTo(title);
+        assertThat(book.getAuthor()).isEqualTo(author);
+        assertThat(book.getPublicationYear()).isEqualTo(publicationYear);
+        assertThat(book.getIsbn()).isEqualTo(isbn);
+    }
+}
