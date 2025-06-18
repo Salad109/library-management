a3584f04ca1f18dafd47d4dcce06d32660651199
Salad109 <szolek1337@gmail.com>
2025-06-18 15:54:26 +0200

    Add unit tests for Book and BookController classes

---
+++ src/test/java/librarymanagement/controller/BookControllerTest.java
@@ -0,0 +1,22 @@
+package librarymanagement.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testGetAllBooks() throws Exception {
+        mockMvc.perform(get("/api/books")).andExpect(status().isOk());
+    }
+}
