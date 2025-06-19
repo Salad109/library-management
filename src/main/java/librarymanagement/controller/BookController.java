4f0960fe0880eb7e6d478788a935dfc03486a58a
Salad109 <szolek1337@gmail.com>
2025-06-19 16:22:33 +0200

    Prevent author duplication when setting book authors

--- src/main/java/librarymanagement/controller/BookController.java
+++ src/main/java/librarymanagement/controller/BookController.java
@@ -1,20 +1,27 @@
 package librarymanagement.controller;
 
 import jakarta.validation.Valid;
+import librarymanagement.model.Author;
 import librarymanagement.model.Book;
 import librarymanagement.model.DuplicateIsbnException;
+import librarymanagement.repository.AuthorRepository;
 import librarymanagement.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
 
     @GetMapping("/api/books")
     public List<Book> getAllBooks(@RequestParam(required = false) Long id) {
@@ -36,6 +43,18 @@ public class BookController {
         if (bookRepository.existsByIsbn(book.getIsbn())) {
             throw new DuplicateIsbnException(book.getIsbn());
         }
+
+        Set<Author> finalAuthors = new HashSet<>();
+        for (Author incomingAuthor : book.getAuthors()){
+            Author existingAuthor = authorRepository.findByName(incomingAuthor.getName());
+            if (existingAuthor != null) {
+                finalAuthors.add(existingAuthor); // if found, use existing author
+            } else {
+                finalAuthors.add(incomingAuthor); // else, add the new author
+            }
+        }
+        book.setAuthors(finalAuthors);
+
         return bookRepository.save(book);
     }
 }
