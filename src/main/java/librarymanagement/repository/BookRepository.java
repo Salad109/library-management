cb250ad9ac2b6f419584c15a37cd707ede7464a2
Salad109 <szolek1337@gmail.com>
2025-06-23 18:42:48 +0200

    Implement pagination for book search, update BookRepository BookController and tests, expand test http requests

--- src/main/java/librarymanagement/repository/BookRepository.java
+++ src/main/java/librarymanagement/repository/BookRepository.java
@@ -1,12 +1,12 @@
 package librarymanagement.repository;
 
 import librarymanagement.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
@@ -14,12 +14,13 @@ public interface BookRepository extends JpaRepository<Book, String> {
             "LEFT JOIN b.authors a " +
             "WHERE (:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))) " +
             "AND (:authorName IS NULL OR LOWER(a.name) LIKE LOWER(CONCAT('%', :authorName, '%'))) " +
-            "AND (:year IS NULL OR b.publicationYear = :year)" +
+            "AND (:year IS NULL OR b.publicationYear = :year) " +
             "AND (:isbn IS NULL OR b.isbn = :isbn)")
-    List<Book> searchBooks(@Param("title") String title,
+    Page<Book> searchBooks(@Param("title") String title,
                            @Param("authorName") String authorName,
                            @Param("year") Integer year,
-                           @Param("isbn") String isbn);
+                           @Param("isbn") String isbn,
+                           Pageable pageable);
 
     Book findByIsbn(String isbn);
 }
