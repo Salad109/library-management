84f2946e4afab906311be67bf36d08a471412b0e
Salad109 <szolek1337@gmail.com>
2025-06-23 17:02:02 +0200

    Add AuthorController and AuthorRepository with GET endpoints

---
+++ src/main/java/librarymanagement/controller/AuthorController.java
@@ -0,0 +1,42 @@
+package librarymanagement.controller;
+
+import librarymanagement.exception.ResourceNotFoundException;
+import librarymanagement.model.Author;
+import librarymanagement.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AuthorController {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @GetMapping("/api/authors")
+    public List<Author> getAuthors() {
+        return authorRepository.findAll();
+    }
+
+    @GetMapping("api/authors/{name}")
+    public Author getAuthorByName(@PathVariable String name) {
+        Author author = authorRepository.findById(name).orElse(null);
+        if (author == null) {
+            throw new ResourceNotFoundException("Author not found with name: " + name);
+        }
+        return author;
+    }
+
+    @GetMapping("/api/authors/search")
+    public List<Author> searchAuthors(@RequestParam(required = false) String name) {
+        if (name == null || name.isBlank()) {
+            return authorRepository.findAll();
+        } else {
+            return authorRepository.findByNameContainingIgnoreCase(name);
+        }
+    }
+}
--- src/main/java/librarymanagement/repository/AuthorRepository.java
+++ src/main/java/librarymanagement/repository/AuthorRepository.java
@@ -4,7 +4,11 @@ import librarymanagement.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, String> {
     Author findByName(String name);
+
+    List<Author> findByNameContainingIgnoreCase(String name);
 }
