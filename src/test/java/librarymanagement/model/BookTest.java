f229c290159e8ac03406e1274f7d28fb7a725be5
Salad109 <szolek1337@gmail.com>
2025-06-28 19:04:46 +0200

    Expand Book constructors; add equality test in BookTest

--- src/test/java/librarymanagement/model/BookTest.java
+++ src/test/java/librarymanagement/model/BookTest.java
@@ -30,4 +30,24 @@ class BookTest {
         assertThat(book.getPublicationYear()).isEqualTo(publicationYear);
         assertThat(book.getIsbn()).isEqualTo(isbn);
     }
+
+    void testBookEquality() {
+        Book book1 = new Book();
+        BookTestData.BookData bookData = BookTestData.getNextBook();
+        book1.setIsbn(bookData.ISBN);
+        book1.setTitle(bookData.TITLE);
+        book1.setPublicationYear(bookData.PUBLICATION_YEAR);
+        book1.getAuthors().add(new Author(bookData.AUTHOR1));
+        book1.getAuthors().add(new Author(bookData.AUTHOR2));
+
+        Book book2 = new Book();
+        book2.setIsbn(bookData.ISBN);
+        book2.setTitle(bookData.TITLE);
+        book2.setPublicationYear(bookData.PUBLICATION_YEAR);
+        book2.getAuthors().add(new Author(bookData.AUTHOR1));
+        book2.getAuthors().add(new Author(bookData.AUTHOR2));
+
+        assertThat(book1.hashCode()).isEqualTo(book2.hashCode());
+        assertThat(book1).isEqualTo(book2);
+    }
 }
