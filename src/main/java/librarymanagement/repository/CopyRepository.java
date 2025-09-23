c38a339a5cd975f41656034fe1b2d201de7d0014
Salad <szolek1337@gmail.com>
2025-09-23 21:24:58 +0200

    Add mark lost functionality to copy page; enhance copy page UI by putting success messages in toast container

--- src/main/java/librarymanagement/repository/CopyRepository.java
+++ src/main/java/librarymanagement/repository/CopyRepository.java
@@ -36,6 +36,6 @@ public interface CopyRepository extends JpaRepository<Copy, Long> {
     @Query("SELECT c.id FROM Copy c WHERE c.book.title ILIKE CONCAT('%', :title, '%') ORDER BY c.id")
     Page<Long> findIdsByBookTitle(@Param("title") String title, Pageable pageable);
 
-        @Query("SELECT c.id FROM Copy c WHERE c.customer.lastName ILIKE CONCAT('%', :lastName, '%') ORDER BY c.id")
+    @Query("SELECT c.id FROM Copy c WHERE c.customer.lastName ILIKE CONCAT('%', :lastName, '%') ORDER BY c.id")
     Page<Long> findIdsByCustomerLastName(@Param("lastName") String lastName, Pageable pageable);
 }
